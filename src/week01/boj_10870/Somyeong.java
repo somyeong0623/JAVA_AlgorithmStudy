6e8c995ad7229c9d9e7af2681982c4c6d9b61b7a
이소명 <thaud8487@naver.com>
2022-08-05 13:22:29 +0900

    weeok01 패키지 생성

--- src/week01/boj_10870/Somyeong.java
+++ src/week01/boj_10870/Somyeong.java
@@ -1,7 +1,8 @@
-package week01.boj_1234;
+package week01.boj_10870;
 
 public class Somyeong {
+
 	public static void main(String[] args) {
-		System.out.println("hello~ ");
+		System.out.println("hello");
 	}
 }
--- src/week01/boj_1234/CJW_Test.java
+++
@@ -1,8 +0,0 @@
-package week01.boj_1234;
-
-public class CJW_Test {
-	public static void main(String[] args) {
-		int i = 1 + 1; // 소명님 Test
-		System.out.println("Hello World asdf qqt");
-	}
-}
