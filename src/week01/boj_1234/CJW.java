83a9c3a21c0baf347516f75e9e0d98192ed800ac
hyeonslove <cjw.git@gmail.com>
2022-08-05 11:55:21 +0900

    qwer

--- src/week01/boj_1234/CJW.java
+++ src/week01/boj_1234/CJW.java
@@ -2,6 +2,6 @@ package week01.boj_1234;
 
 public class CJW {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World asdf qqt");
     }
 }
