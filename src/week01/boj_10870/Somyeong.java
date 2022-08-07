6a139382042d3f33f1656cd0cb7c73fc631e4fbb
이소명 <thaud8487@naver.com>
2022-08-07 23:21:13 +0900

    somyeong week01

--- src/week01/boj_10870/Somyeong.java
+++ src/week01/boj_10870/Somyeong.java
@@ -1,8 +1,25 @@
 package week01.boj_10870;
 
-public class Somyeong {
+import java.util.Scanner;
 
+public class Somyeong {
+// 10870. 피보나치 수 5
+	static int n;
 	public static void main(String[] args) {
-		System.out.println("hello");
+		
+		Scanner sc= new Scanner(System.in);
+		n=sc.nextInt();
+		
+		System.out.println(func(n));
+	}
+	static int func(int n) {
+		if(n==0)
+			return 0;
+		if(n==1)
+			return 1;
+		if (n==2)
+			return 1;
+		
+		return func(n-2)+func(n-1);
 	}
 }
