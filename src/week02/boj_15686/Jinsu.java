0b308191bffd114e1b6b3b19d97b9705dcae5535
정진수 <zxcv423@naver.com>
2022-08-20 21:22:53 +0900

    0820 commit

---
+++ src/week02/boj_15686/Jinsu.java
@@ -0,0 +1,66 @@
+package week02.boj_15686;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Jinsu {
+	static int N, M, result;
+	static int[][] map;
+	static ArrayList<Point> home, chicken;	//집의 좌표값과 치킨집의 좌표값 저장
+	static boolean[] open;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		map = new int[N][N];
+		home = new ArrayList<>();
+		chicken = new ArrayList<>();
+		
+		for(int i=0; i<N; i++) {
+			for (int j=0; j<N; j++) {
+				map[i][j] = sc.nextInt();
+				
+				if (map[i][j] == 1) //집이면
+					home.add(new Point(i, j));		
+				
+				else if(map[i][j] == 2)	//치킨집이면
+					chicken.add(new Point(i, j));	
+			}
+		}
+		
+		result = Integer.MAX_VALUE;
+		open = new boolean[chicken.size()];
+		dfs(0, 0);
+		System.out.println(result);
+		
+	}
+
+	private static void dfs(int cnt, int start) {
+		if (cnt == M) {
+			int ans = 0;
+			for (int i=0; i<home.size(); i++) {
+				int tmp = Integer.MAX_VALUE;
+				for (int j=0; j<chicken.size(); j++) {
+					if(open[j]) {
+						int distance = Math.abs(home.get(i).x- chicken.get(j).x) + Math.abs(home.get(i).y - chicken.get(j).y);
+						tmp = Math.min(distance, tmp);	//최소거리 저장
+					}
+				}
+				ans += tmp;	// 탐색완료 후 최소거리 저장
+			}
+			result = Math.min(ans, result);	// 
+			return;
+		}
+		
+		for (int i= start; i < chicken.size(); i++) {
+			open[i] = true;
+			dfs(cnt+1, i+1);
+			open[i] = false;
+		}
+	}
+
+}
---
+++ src/week02/boj_1758/Jinsu.java
@@ -0,0 +1,37 @@
+package week02.boj_1758;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Jinsu {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		Integer[] num = new Integer[N];
+		long sum = 0;		//long형태로 안쓰면 틀린다
+		
+		for(int i=0; i<N; i++) {
+			num[i] = sc.nextInt();
+		}
+		
+		Comparator<Integer> com = new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o2-o1;
+			}
+		};
+			
+		Arrays.sort(num, com);
+		
+		for(int i=0; i<N; i++) {
+			if (num[i] - i > 0)
+				sum += (num[i]-i);
+		}
+		
+		System.out.println(sum);
+	}
+
+}
