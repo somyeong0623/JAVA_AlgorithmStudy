67d503ad999db2e95de1733242af2529c0b68033
송지율 <lnacles@naver.com>
2022-10-02 00:45:02 +0900

    1002 commit

--- src/week08/boj_1309/Jul.java
+++ src/week08/boj_1309/Jul.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-
+ 
 // 동물원
 public class Jul {
 	public static void main(String[] args) throws IOException {
